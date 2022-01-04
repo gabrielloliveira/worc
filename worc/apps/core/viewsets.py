57af7d9bd811bfd8dfd15ac0cf9a60d72ea281fb
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2022-01-03 22:09:09 -0300

    feat: add custom permission IsAdminOrReadOnly

--- worc/apps/core/viewsets.py
+++ worc/apps/core/viewsets.py
@@ -1,9 +1,9 @@
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import AllowAny
 
 from worc.apps.core.filtersets import CandidateFilterSet
 from worc.apps.core.models import Candidate
+from worc.apps.core.permissions import IsAdminOrReadOnly
 from worc.apps.core.serializers import CandidateSerializer
 
 
@@ -12,7 +12,7 @@ class CandidateListCreate(generics.ListCreateAPIView):
     API endpoint that allows candidates to be viewed or edited.
     """
 
-    permission_classes = [AllowAny]
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = CandidateSerializer
     filterset_class = CandidateFilterSet
     queryset = Candidate.objects.order_by("-created_at")
@@ -23,7 +23,7 @@ class CandidateRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     API endpoint that allows candidates to be viewed, edited and deleted.
     """
 
-    permission_classes = [AllowAny]
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = CandidateSerializer
     queryset = Candidate.objects.all()
 
